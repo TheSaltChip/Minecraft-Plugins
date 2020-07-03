8ffdf3bd2d6343595f68ed175a7c725f213028e7
TheSaltChip <seb2500@gmail.com>
2020-07-03 23:17:17 +0200

    Gitignore and doctor

---
+++ Doctor/src/io/github/thesaltchip/Doctor/Main.java
@@ -0,0 +1,16 @@
+package io.github.thesaltchip.Doctor;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin{
+	@Override
+	public void onEnable() {
+		this.getCommand("Doctor").setExecutor(new Heal());
+	}
+	
+	@Override
+	public void onDisable() {
+		
+	}
+	
+}
